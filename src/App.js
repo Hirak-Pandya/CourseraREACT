b026bc1e27d15ff9a0a92af1aa908db34a7e7a96
Hirak-Pandya <hirakpandya70@gmail.com>
2020-06-13 14:03:02 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import "./App.css";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
 import { DISHES } from "./shared/dishes";
-import Main from './components/MainComponent';
+import Main from "./components/MainComponent";
 
 class App extends Component {
   constructor(props) {
@@ -13,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Main />
+        <Main />
       </div>
     );
   }
